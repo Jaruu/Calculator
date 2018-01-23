19c480defca89729cd450301d7c36862f993a850
Jaroslaw Surma <jarek.surma@gmail.com>
2018-01-23 18:43:22 +0100

    Division by zero added to the Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,6 +12,9 @@ public class Calculator {
     }
 
     public float by(float a, float b) {
+        if (b == 0f) {
+            throw new IllegalArgumentException("Division by 0!");
+        }
         return a / b;
     }
 
