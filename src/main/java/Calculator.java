0b681a5d8f8f04b182fc4a3550a7277edfb7be9f
Jaroslaw Surma <jarek.surma@gmail.com>
2018-01-23 18:19:04 +0100

    Calculator2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+
+    public float minus(float a, float b) {
+        return a - b;
+    }
+
+    public float times(float a, float b) {
+        return a * b;
+    }
+
+    public float by(float a, float b) {
+        return a / b;
+    }
+
 }
